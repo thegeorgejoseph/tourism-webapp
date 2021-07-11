3a519da9fd5e8eeb456fca89a70033e8398e14e8
George Joseph <thegeorgejoseph@gmail.com>
2021-07-11 18:45:07 +0530

    Adding PATCH/tour and DEL/tour + refactoring code

--- app.js
+++ app.js
@@ -9,25 +9,29 @@ const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: { tours: tours },
   });
-});
+};
 
-app.get('/api/v1/tours/:id', (req, res) => {
-  const tour = tours.find((el) => el.id === parseInt(req.params.id));
-  res.status(201).json({
+const getTour = (req, res) => {
+  const id = parseInt(req.params.id);
+  const tour = tours.find((el) => el.id === id);
+  if (!tour) {
+    return res.status(404).json({ status: 'failed', message: 'Invalid ID' });
+  }
+  res.status(200).json({
     status: 'success',
     data: {
       tour,
     },
   });
-});
+};
 
-app.post('/api/v1/tours', (req, res) => {
+const postAllTours = (req, res) => {
   //   console.log(req.body);
   newId = tours[tours.length - 1].id + 1;
   const newTour = { id: newId, ...req.body };
@@ -48,7 +52,36 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
+};
+
+const patchTour = (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    data: {
+      tour: 'UpdatedTourGoesHere',
+    },
+  });
+};
+
+const deleteTour = (req, res) => {
+  if (parseInt(req.params.id) > tours.length) {
+    return res.status(404).json({ status: 'failed', message: 'invalid ID' });
+  }
+  res.status(204).json({
+    staus: 'success',
+    data: null,
+  });
+};
+
+// app.get('/api/v1/tours', getAllTours);
+// app.post('/api/v1/tours', postAllTours);
+// app.get('/api/v1/tours/:id', getTour);
+// app.patch('/api/v1/tours/:id', patchTour);
+// app.delete('/api/v1/tours/:id', deleteTour);
+
+app.route('/api/v1/tours').get(getAllTours).post(postAllTours);
+app.route('/api/v1/tours/:id').get(getTour).patch(patchTour).delete(deleteTour);
+
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
