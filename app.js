cfab444eb13cd093d7d970f2b9eb57816aa174e4
George Joseph <thegeorgejoseph@gmail.com>
2021-07-11 17:40:06 +0530

    setting up GET tours route

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
 const express = require('express');
-
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => {
-  res.status(200).send('Hello from the status side');
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
+);
+
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    status: 'success',
+    results: tours.length,
+    data: { tours: tours },
+  });
 });
+
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
