97ff66eedf3247c53bce4e6d9c51de5e238d0a4b
George Joseph <thegeorgejoseph@gmail.com>
2021-07-11 18:12:11 +0530

    setting up routes for POST/tours and get/toursById

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
+const { ESRCH } = require('constants');
 const express = require('express');
 const fs = require('fs');
 const app = express();
+app.use(express.json());
 const port = 3000;
 
 const tours = JSON.parse(
@@ -15,6 +17,38 @@ app.get('/api/v1/tours', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours/:id', (req, res) => {
+  const tour = tours.find((el) => el.id === parseInt(req.params.id));
+  res.status(201).json({
+    status: 'success',
+    data: {
+      tour,
+    },
+  });
+});
+
+app.post('/api/v1/tours', (req, res) => {
+  //   console.log(req.body);
+  newId = tours[tours.length - 1].id + 1;
+  const newTour = { id: newId, ...req.body };
+  tours.push(newTour);
+  fs.writeFile(
+    `${__dirname}/dev-data/data/tours-simple.json`,
+    JSON.stringify(tours),
+    (err) => {
+      if (err) {
+        throw err;
+      }
+      res.status(201).json({
+        status: 'success',
+        result: tours.length,
+        data: {
+          tours,
+        },
+      });
+    }
+  );
+});
 app.listen(port, () => {
   console.log(`App running on port ${port}...`);
 });
