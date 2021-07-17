6e43aa58d6ec14c312d3e5c639bb31cba98fafcc
George Joseph <thegeorgejoseph@gmail.com>
2021-07-17 19:33:29 +0530

    creating a script to read json from file and update the DB

---
+++ dev-data/data/import-dev-data.js
@@ -0,0 +1,53 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const fs = require('fs');
+const Tour = require('../../models/tourModels');
+
+dotenv.config({ path: '../../config.env' });
+
+const DB = process.env.DATABASE.replace(
+  `<PASSWORD>`,
+  process.env.DATABASE_PASSWORD
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+  })
+  .then(() =>
+    console.log('Database connection for injecting tours successful!')
+  );
+
+const tours = JSON.parse(
+  fs.readFileSync(`${__dirname}/tours-simple.json`, 'utf-8')
+);
+
+const importData = async () => {
+  try {
+    await Tour.create(tours);
+    console.log('Data Successfully loaded!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+const deleteData = async () => {
+  try {
+    await Tour.deleteMany();
+    console.log('Data successfully deleted!');
+  } catch (err) {
+    console.log(err);
+  }
+  process.exit();
+};
+
+console.log(process.argv);
+
+if (process.argv[2] === '--import') {
+  importData();
+} else if (process.argv[2] === '--delete') {
+  deleteData();
+}
