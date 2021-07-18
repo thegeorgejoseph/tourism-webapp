2fbd3dd1431c1000aa8d6d4fe488d6c70f148046
George Joseph <thegeorgejoseph@gmail.com>
2021-07-18 18:07:18 +0530

    Implementing Pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -29,6 +29,22 @@ exports.getAllTours = async (req, res) => {
     } else {
       query = query.sort('-createdAt');
     }
+    if (req.query.fields) {
+      const fields = req.query.fields.split(',').join(' ');
+      query = query.select(fields);
+    } else {
+      query = query.select('-__v');
+    }
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip > numTours) throw new Error('Page does not exist!');
+    }
+    query = query.skip(skip).limit(limit);
+
     const tours = await query;
     res.status(200).json({
       status: 'success',
