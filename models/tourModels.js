1a27eb4e05bb954b175d316cdedff954d0b1ffe3
George Joseph <thegeorgejoseph@gmail.com>
2021-07-21 23:57:00 +0530

    aggregation middleware with unshift()

--- models/tourModels.js
+++ models/tourModels.js
@@ -74,6 +74,13 @@ tourSchema.post(/^find/, function (docs, next) {
   // console.log(docs);
   next();
 });
+
+//aggregation middleware
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+  console.log(this.pipeline());
+  next();
+});
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
