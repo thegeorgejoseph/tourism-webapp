e775ba96c0d9b4e5ea83919777ec3c5d5ae981b1
George Joseph <thegeorgejoseph@gmail.com>
2021-07-22 20:39:39 +0530

    adding a route to handle invalid route errors

--- app.js
+++ app.js
@@ -12,10 +12,10 @@ if (process.env.NODE_ENV === 'development') {
 app.use(express.json());
 app.use(express.static(`${__dirname}/public`));
 
-app.use((req, res, next) => {
-  console.log('Hello from the middleware!');
-  next();
-});
+// app.use((req, res, next) => {
+//   console.log('Hello from the middleware!');
+//   next();
+// });
 
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
@@ -25,5 +25,10 @@ app.use((req, res, next) => {
 //Mounting
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
-
+app.all('*', (req, res, next) => {
+  res.status(404).json({
+    status: 'failed',
+    message: `Can't find ${req.originalUrl} on this server!`,
+  });
+});
 module.exports = app;
