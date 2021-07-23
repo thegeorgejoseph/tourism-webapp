89dd05aeb89ebe0f8019843c442bac29c38e0209
George Joseph <thegeorgejoseph@gmail.com>
2021-07-23 18:50:00 +0530

    implementing password management using bcrypt

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: { type: String, required: [true, 'Users need a name!'] },
@@ -16,9 +17,26 @@ const userSchema = new mongoose.Schema({
     required: true,
     minlength: [8, 'Password must atleast have 8 characters'],
   },
-  passwordConfirm: { type: String, required: true },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'confirm your password'],
+    validate: {
+      // only works on CREATE or SAVE
+      validator: function (val) {
+        return this.password === val;
+      },
+      message: 'passwords should match',
+    },
+  },
 });
 
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+  next();
+});
 const User = mongoose.model('User', userSchema);
 
 module.exports = User;
