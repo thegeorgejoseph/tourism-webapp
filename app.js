afc93773a1faa14e5f1ecd851ecba01f472ef2dd
George Joseph <thegeorgejoseph@gmail.com>
2021-07-24 23:11:39 +0530

    preventing noSQL injections using mongo-sanitize and cross scripting attack using xss-clean

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
 const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
@@ -26,6 +28,10 @@ app.use('/api', limiter);
 //parsing req.body in right form automatically
 app.use(express.json());
 
+//data sanitization against noSQL injection
+app.use(mongoSanitize());
+//data sanitization against cross side scripting attack
+app.use(xss());
 //serving static files present in the public directory
 app.use(express.static(`${__dirname}/public`));
 
