2a8ac47b3a3eed2866ee87a195464d91409e08e3
George Joseph <thegeorgejoseph@gmail.com>
2021-07-25 18:20:53 +0530

    embedding tour guides into tours

--- models/tourModels.js
+++ models/tourModels.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 // const validator = require('validator');
+const User = require('./userModel');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -89,6 +90,7 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: Array,
   },
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
@@ -103,6 +105,11 @@ tourSchema.pre('save', function (next) {
   next();
 });
 
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
+  next();
+});
 // tourSchema.post('save', (doc, next) => {
 //   console.log(doc);
 //   next();
