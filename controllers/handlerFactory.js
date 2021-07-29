bef86bd4600714643db8730377ff6d5e2e3040f3
George Joseph <thegeorgejoseph@gmail.com>
2021-07-30 00:10:43 +0530

    adding Indexes (Singular + Compound) to improve Read on collection

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -72,6 +72,7 @@ exports.getAll = (Model) =>
       .limitFields()
       .paginate();
 
+    // const doc = await features.query.explain();
     const doc = await features.query.explain();
     res.status(200).json({
       status: 'success',
--- models/tourModels.js
+++ models/tourModels.js
@@ -101,6 +101,9 @@ const tourSchema = new mongoose.Schema(
   { toJSON: { virtuals: true }, toObject: { virtuals: true } }
 );
 
+// tourSchema.index({ price: 1 }); // creating an index for the schema
+tourSchema.index({ price: 1, ratingsAverage: -1 });
+tourSchema.index({ slug: 1 });
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
 });
