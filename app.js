55bff57790d5a0f8408a9f3c30799908849623d4
George Joseph <thegeorgejoseph@gmail.com>
2021-07-31 00:01:40 +0530

    setting up base.pug to dynamically render

--- app.js
+++ app.js
@@ -68,6 +68,10 @@ app.use((req, res, next) => {
 });
 
 //Mounting
+app.get('/', (req, res) => {
+  res.status(200).render('base');
+});
+
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
