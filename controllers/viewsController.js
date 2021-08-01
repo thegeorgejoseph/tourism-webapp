e2189fb15359b40c340d8c01b15ac6700a585952
George Joseph <thegeorgejoseph@gmail.com>
2021-08-02 00:08:02 +0530

    adding pug templates

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -28,7 +28,10 @@ exports.getTour = catchAsync(async (req, res, next) => {
 });
 
 exports.getLoginForm = (req, res, next) => {
-  res.status(200).render('login', {
-    title: 'Log into your account',
-  });
+  res
+    .status(200)
+    .set('Content-Security-Policy', "connect-src 'self' http://127.0.0.1:3001")
+    .render('login', {
+      title: 'Log into your account',
+    });
 };
